b0d603c008f3439d863ff23018f2fd3835991e75
Ashmit <ashmitmala002@gmail.com>
2025-04-20 20:27:51 +0530

    Dsiplay Category Items on a Screen

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -8,6 +8,24 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Text('1');
+    return Container(
+      padding:const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withAlpha((0.55 * 255).round()),
+            category.color.withAlpha((0.9 * 255).round()),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+          color: Theme.of(context).colorScheme.onSurface,
+        ),
+      ),
+    );
   }
 }
