2e562287212115977fae689208819a8ab8c787fe
Ashmit <ashmitmala002@gmail.com>
2025-04-21 22:08:13 +0530

    Add Navigation to the MealDetails Screen

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
-   CategoryGridItem({super.key, required this.category, required this.onSelectCategory});
+  const CategoryGridItem({super.key, required this.category, required this.onSelectCategory});
 
   final Category category;
   final void Function() onSelectCategory;
